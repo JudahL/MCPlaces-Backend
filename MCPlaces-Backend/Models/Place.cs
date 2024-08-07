f5bde143b3ab3503b439bbce7b7b31f0e8d37e34
JudahL <judahdslucas@gmail.com>
2024-08-07 22:07:52 +0100

    Setup project and added Place model

---
+++ MCPlaces-Backend/Models/Place.cs
@@ -0,0 +1,21 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MCPlaces_Backend.Models
+{
+    public class Place
+    {
+        [Key]
+        public int Id { get; set; }
+        //[ForeignKey("Server")]
+        //public int ServerId { get; set; }
+        //public MCServer Server { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public string ImageName { get; set; } = String.Empty;
+        public int CoordsX { get; set; }
+        public int CoordsY { get; set; }
+        public int CoordsZ { get; set; }
+    }
+}
