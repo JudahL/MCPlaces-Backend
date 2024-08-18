05ba1978ec75aa9ae681044196ee16f5f5560da9
JudahL <judahdslucas@gmail.com>
2024-08-19 00:00:37 +0100

    Added Servers table to database

--- MCPlaces-Backend/Models/Server.cs
+++ MCPlaces-Backend/Models/Server.cs
@@ -8,8 +8,10 @@ namespace MCPlaces_Backend.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = String.Empty;
         public string Description { get; set; } = String.Empty;
         public string Patch { get; set; } = String.Empty;
+        public string Seed { get; set; } = String.Empty;
     }
 }
