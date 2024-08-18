ea69c2eb293df0ccf25ce35bc2fdd7ce9069e9c1
JudahL <judahdslucas@gmail.com>
2024-08-18 23:28:08 +0100

    Fixed nullability issue in Repository Get/GetAll Arguments

--- MCPlaces-Backend/Repository/GenericRepository/Interfaces/IRepository.cs
+++ MCPlaces-Backend/Repository/GenericRepository/Interfaces/IRepository.cs
@@ -6,7 +6,7 @@ namespace MCPlaces_Backend.Repository.GenericRepository.Interfaces
     public interface IRepository<T> where T : class
     {
         Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null);
-        Task<T> GetAsync(Expression<Func<T, bool>> filter = null);
+        Task<T?> GetAsync(Expression<Func<T, bool>>? filter = null);
         Task CreateAsync(T entity);
         Task<T> UpdateAsync(T entity);
         Task RemoveAsync(T entity);
