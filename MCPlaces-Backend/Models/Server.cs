3bd29bd620343ad684f869009f36a4abd246ff33
JudahL <judahdslucas@gmail.com>
2024-08-18 23:20:11 +0100

    Added Server model and repository

---
+++ MCPlaces-Backend/Models/Server.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MCPlaces_Backend.Models
+{
+    public class Server
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public string Patch { get; set; } = String.Empty;
+    }
+}
