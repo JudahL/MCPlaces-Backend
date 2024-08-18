7d5ea8424df6e9b361529f57ac1de51688ddfc1f
JudahL <judahdslucas@gmail.com>
2024-08-19 00:15:22 +0100

    Added Server data transfer objects

---
+++ MCPlaces-Backend/Models/Dtos/GetServerDto.cs
@@ -0,0 +1,19 @@
+﻿namespace MCPlaces_Backend.Models.Dtos
+{
+    public class GetServerDto
+    {
+        public GetServerDto(int id, string name, string description, string patch, string seed)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Patch = patch;
+            Seed = seed;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public string Patch { get; set; } = String.Empty;
+        public string Seed { get; set; } = String.Empty;
+    }
+}
---
+++ MCPlaces-Backend/Models/Dtos/UpdateServerDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MCPlaces_Backend.Models.Dtos
+{
+    public class UpdateServerDto
+    {
+        public UpdateServerDto(int id, string name, string description, string patch, string seed)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Patch = patch;
+            Seed = seed;
+        }
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public string Patch { get; set; } = String.Empty;
+        public string Seed { get; set; } = String.Empty;
+    }
+}
