00d8e08bd54d6a13fb4883dd27ab804100a5742b
JudahL <judahdslucas@gmail.com>
2024-08-19 15:16:44 +0100

    Added Server Api Controller and Server Mapper

--- MCPlaces-Backend/Program.cs
+++ MCPlaces-Backend/Program.cs
@@ -3,6 +3,8 @@ using MCPlaces_Backend.Models.ApiResponse;
 using MCPlaces_Backend.Models.ApiResponse.Interfaces;
 using MCPlaces_Backend.Repository.PlaceRepository;
 using MCPlaces_Backend.Repository.PlaceRepository.Interfaces;
+using MCPlaces_Backend.Repository.ServerRepository;
+using MCPlaces_Backend.Repository.ServerRepository.Interfaces;
 using MCPlaces_Backend.Utilities.ActionFilters;
 using MCPlaces_Backend.Utilities.ActionFilters.Interfaces;
 using MCPlaces_Backend.Utilities.Mappers;
@@ -13,19 +15,28 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<ApplicationDbContext>(option => {
+builder.Services.AddDbContext<ApplicationDbContext>(option => 
+{
     option.UseSqlite(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
 });
+
+// Places Dependencies.
 builder.Services.AddScoped<IPlaceRepository, PlaceRepository>();
 builder.Services.AddScoped<IPlaceMapper, PlaceMapper>();
+// Server Dependencies.
+builder.Services.AddScoped<IServerRepository, ServerRepository>();
+builder.Services.AddScoped<IServerMapper, ServerMapper>();
+// General Dependencies.
 builder.Services.AddScoped<IApiResponse, ApiResponse>();
 builder.Services.AddScoped<IModelValidationActionFilter, ModelValidationActionFilter>();
 
 builder.Services.AddControllers()
     .AddNewtonsoftJson();
 
-builder.Services.Configure<ApiBehaviorOptions>(options
-    => options.SuppressModelStateInvalidFilter = true);
+builder.Services.Configure<ApiBehaviorOptions>(options => 
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
